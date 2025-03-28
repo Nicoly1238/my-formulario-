38ac0a2c3ad878a262040d8597bf62908d1ae4f6
Nicoly1238 <nicoly.cardoso120421@gmail.com>
2025-03-28 01:25:10 -0300

    Add files via upload

--- processa.php
+++ processa.php
@@ -4,24 +4,30 @@ $username = "root";
 $password = "";
 $dbname = "loja_virtual";
 
+// Cria conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica conexão
 if ($conn->connect_error) {
-  die("Falha de conexão: " . $conn->connect_error);
+    die("Conexão falhou: " . $conn->connect_error);
 }
 
+// Pega os dados do formulário
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $idade = $_POST['idade'];
 $cidade = $_POST['cidade'];
 $estado = $_POST['estado'];
 
-$sql = "INSERT INTO clientes (NOME, EMAIL, IDADE, CIDADE, ESTADO)
+// Prepara o SQL
+$sql = "INSERT INTO clientes (nome, email, idade, cidade, estado)
 VALUES ('$nome', '$email', '$idade', '$cidade', '$estado')";
 
+// Executa e mostra resultado
 if ($conn->query($sql) === TRUE) {
-  echo "Cadastro realizado com sucesso!";
+    echo "<h2 style='color: #e14e6b; text-align: center; font-family: Poppins, sans-serif; margin-top: 50px;'>Awn! Obrigada por preencher, gatinho! Seus dados foram salvos com sucesso!</h2>";
 } else {
-  echo "Erro: " . $sql . "<br>" . $conn->error;
+    echo "Erro: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
